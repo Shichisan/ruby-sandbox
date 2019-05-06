4f7e47739b82539d7f0e6a030275c5b2f921dd98
Shichisan <taiga.shichisan@gmail.com>
2019-05-06 17:26:56 +0900

    implement interactors and dependency injection
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- hanami/bookshelf/lib/bookshelf/interactors/add_book.rb
+++ hanami/bookshelf/lib/bookshelf/interactors/add_book.rb
@@ -3,9 +3,13 @@ require 'hanami/interactor'
 class AddBook
   include Hanami::Interactor
 
-  def initialze
+  expose :book
+
+  def initialize(repository: BookRepository.new)
+    @repository = repository
   end
 
   def call(book_attributes)
+    @book = @repository.create(book_attributes)
   end
 end
